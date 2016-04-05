866d55796e92a997b884e8626b6b7a8008a4f7fe
HWliao <499606788@qq.com>
2016-04-05 17:37:47 +0800

    ex01 completed

---
+++ src/main/java/com/demo/mytomcat/ex01/HttpServer.java
@@ -0,0 +1,82 @@
+package com.demo.mytomcat.ex01;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+public class HttpServer {
+	/**
+	 * web资源根路径
+	 */
+	public static final String WEB_ROOT = "target" + File.separator + "classes"
+			+ File.separator + "webroot";
+	
+	
+	/**
+	 * shutdown command
+	 */
+	private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+	
+	/**
+	 * shut down command received
+	 */
+	private boolean shutdown = false; 
+	
+	public static void main(String[] args) {
+		HttpServer server = new HttpServer();
+		server.await();
+	}
+	
+	public void await(){
+		ServerSocket serverSocket = null;
+		int port = 8080;
+		try {
+			serverSocket = new ServerSocket(port,1,InetAddress.getByName("127.0.0.1"));
+		} catch ( IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		// loop waiting for a request
+		while (!this.shutdown) {
+			Socket socket = null;
+			InputStream input = null;
+			OutputStream output = null;
+			try {
+				socket = serverSocket.accept();
+				input = socket.getInputStream();
+				output = socket.getOutputStream();
+				// create a request object
+				Request request = new Request(input);
+				request.parse();
+
+				// create a response object
+				Response response = new Response(output);
+				response.setRequest(request);
+				response.sendStaticResource();
+				// close socket
+				socket.close();
+				// check if the previouus uri is shutdown command
+				shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+			} catch (Exception e) {
+				e.printStackTrace();
+				continue;
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
